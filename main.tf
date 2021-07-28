363a18854cf68b8a8156fadfb6b10fc7cc15158c
hiroshi-kato <hiroshikato1208@gmail.com>
2021-07-28 10:06:09 +0900

    3章6まで

--- main.tf
+++ main.tf
@@ -2,9 +2,51 @@ provider "aws" {
   region  = "ap-northeast-1"
   profile = "terraform_sample"
 }
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
+locals {
+  example_instance_type = "t3.nano"
+}
+
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+}
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = local.example_instance_type
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   user_data = <<EOF
     #!/bin/bash
@@ -15,4 +57,8 @@ resource "aws_instance" "example" {
   tags = {
     "Name" = "example"
   }
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.public_dns
 }
